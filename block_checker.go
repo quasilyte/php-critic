270c770953cc0c8fb809c1dd265ddb02404bfda2
Iskander Sharipov <quasilyte@gmail.com>
2019-03-19 21:22:26 +0300

    add third "define" argument checker
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- block_checker.go
+++ block_checker.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/VKCOM/noverify/src/linter"
+	"github.com/VKCOM/noverify/src/meta"
+	"github.com/z7zmey/php-parser/node/expr"
+	"github.com/z7zmey/php-parser/node/name"
 	"github.com/z7zmey/php-parser/walker"
 )
 
@@ -13,4 +16,26 @@ func (c *blockChecker) AfterEnterNode(w walker.Walkable)  {}
 func (c *blockChecker) BeforeLeaveNode(w walker.Walkable) {}
 func (c *blockChecker) AfterLeaveNode(w walker.Walkable)  {}
 
-func (c *blockChecker) BeforeEnterNode(w walker.Walkable) {}
+func (c *blockChecker) BeforeEnterNode(w walker.Walkable) {
+	switch n := w.(type) {
+	case *expr.FunctionCall:
+		c.handleFunctionCall(n)
+	}
+}
+
+func (c *blockChecker) handleFunctionCall(call *expr.FunctionCall) {
+	name, ok := call.Function.(*name.Name)
+	if !ok {
+		return
+	}
+
+	if meta.NameEquals(name, "define") {
+		c.checkDefine(call)
+	}
+}
+
+func (c *blockChecker) checkDefine(define *expr.FunctionCall) {
+	if len(define.Arguments) != 2 {
+		c.ctxt.Report(define.Arguments[2], linter.LevelWarning, "don't use case_insensitive argument")
+	}
+}
--- checkers_test.go
+++ checkers_test.go
@@ -1,1 +1,129 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/VKCOM/noverify/src/linter"
+	"github.com/VKCOM/noverify/src/meta"
+	"github.com/z7zmey/php-parser/node"
+)
+
+func TestDefineArg3(t *testing.T) {
+	reports := multiFileReports(t, `<?php
+	/** @linter disable */
+	function define() {}
+	define("true", 1 === 1);
+	define("false", 1 === 0);
+	`, `<?php
+	define("THE_CONST_TRUE", 1, true);
+	define("THE_CONST_FALSE", 0, false);
+	`)
+	matchReports(t, reports,
+		`don't use case_insensitive argument`,
+		`don't use case_insensitive argument`)
+}
+
+func singleFileReports(t *testing.T, contents string) []*linter.Report {
+	meta.ResetInfo()
+
+	testParse(t, `first.php`, contents)
+	meta.SetIndexingComplete(true)
+	_, w := testParse(t, `first.php`, contents)
+
+	return w.GetReports()
+}
+
+// multiFileReports is like singleFileReports, but permits several file sources.
+//
+// This is handy when some definitions should be handled separately.
+// Since the main usage for that is disabling warnings for separate sources,
+// the /** @linter disable */ comment is supported.
+func multiFileReports(t *testing.T, contentsList ...string) []*linter.Report {
+	meta.ResetInfo()
+	for i, contents := range contentsList {
+		testParse(t, fmt.Sprintf("file%d.php", i), contents)
+	}
+	meta.SetIndexingComplete(true)
+	var reports []*linter.Report
+	for i, contents := range contentsList {
+		if strings.Contains(contents, "/** @linter disable */") {
+			// Mostly used to add builtin definitions
+			// and for other kind of stub code that was
+			// inserted to make actual testing easier (or possible, even).
+			continue
+		}
+		_, w := testParse(t, fmt.Sprintf("file%d.php", i), contents)
+		reports = append(reports, w.GetReports()...)
+	}
+	return reports
+}
+
+// matcheReports tries to assert that all reports are matched by the expected list.
+// Report entry is matched if it contains any of the expected strings.
+//
+// The order in the expect list doesn't matter, it acts like a set.
+//
+// Every "expect" string can match only once.
+// If there are multiple repeated (same text) report messages to be matched,
+// they must be duplicated.
+func matchReports(t *testing.T, reports []*linter.Report, expect ...string) {
+	for _, r := range reports {
+		log.Printf("%s", r)
+	}
+
+	if len(reports) != len(expect) {
+		t.Errorf("Unexpected number of reports: expected %d, got %d",
+			len(expect), len(reports))
+	}
+
+	matchedReports := map[*linter.Report]bool{}
+	usedMatchers := map[int]bool{}
+	for _, r := range reports {
+		have := r.String()
+		for i, want := range expect {
+			if usedMatchers[i] {
+				continue
+			}
+			if strings.Contains(have, want) {
+				matchedReports[r] = true
+				usedMatchers[i] = true
+				break
+			}
+		}
+	}
+	for i, r := range reports {
+		if matchedReports[r] {
+			continue
+		}
+		t.Errorf("unexpected report %d: %s", i, r.String())
+	}
+	for i, want := range expect {
+		if usedMatchers[i] {
+			continue
+		}
+		t.Errorf("pattern %d matched nothing: %s", i, want)
+	}
+}
+
+var once sync.Once
+
+func testParse(t *testing.T, filename string, contents string) (rootNode node.Node, w *linter.RootWalker) {
+	once.Do(func() { go linter.MemoryLimiterThread() })
+
+	var err error
+	rootNode, w, err = linter.ParseContents(filename, []byte(contents), "UTF-8", nil)
+	if err != nil {
+		t.Errorf("Could not parse %s: %s", filename, err.Error())
+		t.Fail()
+	}
+
+	if !meta.IsIndexingComplete() {
+		w.UpdateMetaInfo()
+	}
+
+	return rootNode, w
+}
